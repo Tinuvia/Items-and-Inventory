9e872f31bd0caf2a340b22ec0c5f3ade51462bf1
TinuviaWV <71916978+Tinuvia@users.noreply.github.com>
2021-07-06 11:40:39 +0200

    implements itemSlots

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -14,5 +14,24 @@ public class Inventory : MonoBehaviour
         {
             itemSlots = itemsParent.GetComponentsInChildren<ItemSlot>();
         }
+
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+        // matches our itemSlots (UI-element) with our itemList (items)
+    {
+        int i = 0;
+        for (; (i < items.Count && i < itemSlots.Length); i++)
+            //every item gets assigned to an item-slot
+        {
+            itemSlots[i].Item = items[i];
+        }
+
+        for (; (i < itemSlots.Length); i++)
+            // for any item-slot that doesn't have an item to go into it, set to null
+        {
+            itemSlots[i].Item = null;
+        }
     }
 }
