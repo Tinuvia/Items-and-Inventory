07f6eece319207e4d909e1b15946c9ea11f6928d
TinuviaWV <71916978+Tinuvia@users.noreply.github.com>
2021-07-20 16:24:09 +0200

    tut 8 - instantiating scriptable objects

--- Assets/Scripts/CraftingSystem/IItemContainer.cs
+++ Assets/Scripts/CraftingSystem/IItemContainer.cs
@@ -2,8 +2,8 @@ public interface IItemContainer
 {
     // an interface allows decoupling of the inventory itself and any systems that use it
 
-    int ItemCount(Item item);
-    bool ContainsItem(Item item);
+    int ItemCount(string itemID);
+    Item RemoveItem(string itemID);
     bool RemoveItem(Item item);
     bool AddItem(Item item);
     bool IsFull();
